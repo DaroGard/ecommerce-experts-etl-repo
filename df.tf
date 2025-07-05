630c55acbb65b7a3b9d662f3e91d1e6d2638fc24
DaroGard <alexdg997@gmail.com>
2025-07-04 22:44:35 -0600

    minor fixes

--- df.tf
+++ df.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_factory" "etl" {
-    name = "df-${var.project}-${var.enviroment}-1234"
+    name = "df-${var.project}-${var.enviroment}"
     location = var.location
     resource_group_name = data.azurerm_resource_group.rg.name
 
